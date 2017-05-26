bb1ef3b401afc17fd59eb647f21c763ee0c7a37e
Douglas Balkin <douglasbalkin@Douglass-MacBook-Pro.local>
2017-05-25 23:13:38 -0500

    updated JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,53 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+// $(document).ready(function() {
+//     console.log( "ready!" );
 
 // Variables
 
-var rNumber = ' ';
+var numbers = [1,2,3,4,5,6,7,8,9,10,11,12];
+var finalScore = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+            42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+            71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+            100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
 var wins = 0;
 var losses = 0;
-var pScore = 0;
+var playerScore = 0;
+var pokeBall = Math.floor(Math.random() * numbers.length);
+var yoshiEgg = Math.floor(Math.random() * numbers.length);
+var mushroom = Math.floor(Math.random() * numbers.length);
+var coinBox = Math.floor(Math.random() * numbers.length);
+var computerScore = Math.floor(Math.random() * finalScore.length);
+
 
 // Functions
 
-// Reset
-
-
 // Clink Events for icons
-$( "#image1" ).click(function randomIntFromInterval(min,max)
-{
-	var img1 = "";
-
-	for (var i =0; i < 2; i++){
-		var random = Math.floor(Math.random()*(max-min+1)+min);}
-		
-		img1 = random + img1;
-
-	console.log(1,12);
+$( "#pokeball" ).click(function() {
+	playerScore = playerScore + pokeBall;
+	$("#playerScore").text(playerScore);
 });
 
-$( "#image2" ).click(function() {
-  alert( "Handler for .click() called." );
+$( "#yoshiEgg" ).click(function() {
+ 	console.log(yoshiEgg);
 });
 
-$( "#image3" ).click(function() {
-  alert( "Handler for .click() called." );
+$( "#mushroom" ).click(function() {
+	console.log(mushroom); 
 });
 
-$( "#image4" ).click(function() {
-  alert( "Handler for .click() called." );
+$( "#coinBox" ).click(function() {
+	console.log(coinBox); 
 });
 
-})
+$("#computerScore").text(computerScore);
+
+
+
+
+
+
+
+
+
+
 
 
